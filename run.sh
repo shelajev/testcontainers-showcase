19637d391bd5779adb8947c0bb4e02da8501acd9
Oleg Šelajev <shelajev@gmail.com>
2023-01-05 22:02:53 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-mvn dependency:get -DremoteRepositories=jitpack::default::https://jitpack.io -Dartifact=com.github.shelajev:maven-profiler:main-c980368781-1
-mvn -Dmaven.ext.class.path=~/.m2/repository/com/github/shelajev/maven-profiler/main-c980368781-1/maven-profiler-main-c980368781-1.jar test -DforkCount=4
